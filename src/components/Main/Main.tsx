77174f7ef0daf6099855846cdadb78a2cb6084ca
Prosper <enweremproper@gmail.com>
2024-01-29 13:43:50 +0100

    Finished with Main Component

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,33 +1,38 @@
 // import { KUTE } from "./../../kute.js";
 function Main() {
   return (
-    <div className="main curveGradient">
-      {/* <BlobAnimation /> */}
-      <div className="main__content">
-        <div>
-          <div className="main__heading">
-            <h1 className="main__title">Hello, My name is</h1>
-            <span className="main__name">Prosper Enwerem</span>
+    <div className="main ">
+      <div className="main__content container-curve">
+        <div className="main__heading">
+          <h1 className="main__title">Hello, My name is</h1>
+          <span className="main__name">Prosper Enwerem</span>
+        </div>
+        <BlobAnimation />
+        <div className="main__info">
+          <h2 className="main__profession">I am a Developer</h2>
+          <p className="main__description">
+            I am not just a Full Stack Web Developer; I am a digital creator
+            passionate about bringing visions to life. With a keen eye for
+            aesthetics and a dedication to user-centric and SEO-friendly design,
+            I specialize in creating websites that not only look good but also
+            perform exceptionally well. From crafting simple static sites to
+            managing intricate blog platforms and building scalable agency
+            websites, I am committed to delivering digital solutions that leave
+            a lasting impact.
+          </p>
+          <div className="main__actions">
+            <a href="#resume" className="btn btn--twist">
+              Download CV
+            </a>
+            <button className="btn btn--twist main__cta">Hire Me</button>
           </div>
         </div>
-
-        <h2 className="main__profession">I am a Developer</h2>
-        <p className="main__description">
-          I am not just a Full Stack Web Developer; I am a digital creator
-          passionate about bringing visions to life. With a keen eye for
-          aesthetics and a dedication to user-centric and SEO-friendly design, I
-          specialize in creating websites that not only look good but also
-          perform exceptionally well. From crafting simple static sites to
-          managing intricate blog platforms and building scalable agency
-          websites, I am committed to delivering digital solutions that leave a
-          lasting impact.
-        </p>
       </div>
-      <div className="main__cta">
-        <a href="#resume" className="btn btn--twist">
-          Download CV
-        </a>
-        <button className="btn btn--twist cta">Hire Me</button>
+      <div className="main__dividers">
+        <div>
+          <div className="main__divider"></div>
+          <div className="main__divider"></div>
+        </div>
       </div>
     </div>
   );
@@ -110,7 +115,11 @@ function BlobAnimation() {
           />
         </g>
       </svg> */}
-      <img src="./static/profile.png" alt="Profile Picture" />
+      <img
+        src="./static/profile.png"
+        alt="Profile Picture"
+        // style={{ width: "100%" }}
+      />
     </div>
   );
 }
